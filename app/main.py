f3dc7057563f9f2ee222e7fbb8076b414425aefc
Sriram Jayashankar <sriramjayashankar@gmail.com>
2024-04-09 22:50:35 +0530

    refactored directory

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from typing import Optional
 from fastapi.params import Body
 import psycopg2
 import time
-from .routes import posts,user
+from . import posts,user
 # posts=[{"title":"Blog 1", "content":"Content of Blog 1","id":1},{"title":"Blog 2", "content":"Content of Blog 2","id":2}]#created a list of dictionaries to temporarily act as a database
 
 
@@ -15,33 +15,30 @@ from .routes import posts,user
 
 
 # to ensure that it connects and if thres any issue put that onto the screen we use a while loop and a time.sleep function
-while(True):
-    try:
-        conn=psycopg2.connect("dbname=fastapiDatabase user=postgres password=builafisory")
-        break
-    except Exception as e:
-        print(f"error while connecting\n{e}")
-    time.sleep(5)
 
-cursor=conn.cursor()
-cursor.execute("""select * from posts""")
-records=cursor.fetchall()
-print(records)
+def connecttodb():
+    while(True):
+        try:
+            conn=psycopg2.connect("dbname=fastapiDatabase user=postgres password=builafisory")
+            break
+        except Exception as e:
+            print(f"error while connecting\n{e}")
+        time.sleep(5)
+
+# cursor=conn.cursor()
+# cursor.execute("""select * from posts""")
+# records=cursor.fetchall()
+# print(records)
 #function to read an id of the post
 
 #if i make something optional i need to give it a default value because there is no id in the body of the request it ,and i add it later 
 
-
-app=FastAPI()
+def main():
+    connecttodb()
+    app=FastAPI()
 #     return {"data":hello}
+    app.include_router(posts.APIRouter)
+    app.include_router(user.APIRouter)
 
-app.include_router(posts.APIRouter)
-app.include_router(user.APIRouter)
-
-@app.post("/testpost")
-async def testfun(request: Request):
-    try:
-        json= await request.json()
-        return {"data":json}
-    except(e):
-        return{"erdggdgror":e}
+def __init__():
+    main()
