d24cacbd7455c0601f10141406957b665082f458
Sriram Jayashankar <sriramjayashankar@gmail.com>
2024-04-11 23:16:46 +0530

    added databse in utils

---
+++ app/utils/databse.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+def connecttodb():
+    while(True):
+        try:
+            conn=psycopg2.connect("dbname=fastapiDatabase user=postgres password=builafisory")
+            break
+        except Exception as e:
+            print(f"error while connecting\n{e}")
+        time.sleep(5)
+    cursor=conn.cursor()
+    return (conn,cursor)
